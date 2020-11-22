5b85987ddaed315f7f8a88a77f5f42a091e5f1e5
rschwarzler <rory.schwarzler@kpn.com>
2020-11-22 13:22:31 +0100

    Assignment 1 code

--- reactjs/src/complete/App.js
+++ reactjs/src/complete/App.js
@@ -5,10 +5,22 @@ import work from './data/work.json';
 import education from './data/education.json';
 import hobbies from './data/hobbies.json';
 
+function starRating(number) {
+  let stars = '';
+  for (let index = 1; index <= number; index++) {
+    stars += '⭐';
+  }
+  return stars;
+}
+
+function convertToUpperCase(company = '') {
+  return company.toUpperCase();
+}
+
 function getWork(items) {
   return items.map((job, index) => {
       const {id, role, company, location, skills, period} = job;
-      const allCapsCompany = company.toUpperCase();
+      const allCapsCompany = convertToUpperCase(company);
 
       return (
         <div className="feature job" key={id}>
@@ -39,13 +51,14 @@ function getEducation(items) {
 
 // Custom React Element
 const Hobbies = (props) => {
-  const {hobbies} = props;
-  const renderHobbies = hobbies.items.map((hobby, index) => {
-    const {id, title, passion} = hobby;
+  const {listOfHobbies} = props;
+  const renderHobbies = listOfHobbies.items.map((hobby, index) => {
+    const {id, title, passion, description} = hobby;
 
     return (
       <div className="feature hobby" key={id}>
-        <p>{title} {passion}/5</p>
+        <p><b>{title}</b> {starRating(passion)}</p>
+        <p>{description}</p>
       </div>
     );
   });
@@ -57,48 +70,47 @@ const Hobbies = (props) => {
     <>
       <h2>Hobbies</h2>
       <section className="hobbies">
-        <p>Total passion: {totalRating}</p>
-        { renderHobbies }
+        {renderHobbies}
+        <span>Total passion: {totalRating} ⭐</span><br/>
       </section>
     </>
   );
 }
 
 function App() {
-  return (
+    return (
     <div className="app">
       <header>
         <h2>Welcome to my online portfolio</h2>
       </header>
 
       <section className="intro">
-        {/* Hint: Use paragraph tags <p> */}
-
-        <p>{ intro.aboutMe }</p>
-        <p>{ intro.description }</p>
-        <p>{ intro.welcomeMessage }</p>
-        <p>{ intro.goal }</p>
+        <p>{intro.aboutMe}</p>
+        <p>I am {intro.age} years old, which is {intro.age * 7} in dog years.</p>
+        <p>{intro.description}</p>
+        <p>{intro.welcomeMessage}</p>
+        <p>{intro.goal}</p>
       </section>
 
       <h2>Some more things about me</h2>
       <section id="details" className="details">
-        <p>I live at { details.address} in the cool town of { details.city }</p>
-        <p>If you need help, you can often call me at <a href={`tel:${details.phoneNumber}`}>{details.phoneNumber}</a> or send an e-mail to <a href={`mailto:${details.emailAddress}`}>{ details.emailAddress }</a></p>
-        <p>Ever seen lorem ipsum? { details.story11 } </p>
-        <p>{ details.story2 } </p>
+        <p>I live at {details.address} in the cool town of {details.city}</p>
+        <p>If you need help, you can often call me at <a href={`tel:${details.phoneNumber}`}>{details.phoneNumber}</a> or send an e-mail to <a href={`mailto:${details.emailAddress}`}>{details.emailAddress}</a></p>
+        <p>Ever seen lorem ipsum? {details.story1} </p>
+        <p>{details.story2} </p>
       </section>
 
       <h2>Work experience</h2>
       <section id="work" className="work">
-        { getWork(work.items) }
+        {getWork(work.items)}
       </section>
 
       <h2>Education</h2>
       <section id="education" className="education">
-        { getEducation(education.items) }
+        {getEducation(education.items)}
       </section>
 
-      <Hobbies hobbies={hobbies} />
+      <Hobbies listOfHobbies={hobbies} />
     </div>
   );
 }
