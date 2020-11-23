f39ac74c9e0c636c8caf38c98cfd604df78417ca
Patrick Kraaij <patrick.kraaij@gmail.com>
2020-11-23 09:50:49 +0100

    Adding some excercises

---
+++ nodejs/src/excercise-types/advanced-types.ts
@@ -0,0 +1,3 @@
+// 1. Create a function which returns a Promise with a string
+
+// 2. Create an interface with some readonly keys and use that as a return interface in your function
---
+++ nodejs/src/excercise-types/interfaces-and-types.ts
@@ -0,0 +1,11 @@
+// 1. Create a function which returns a string
+
+// 2. Create a function which returns true or false
+
+// 3. Create a functions which returns an array of IPersons
+
+// 4. Create a function with the following parameters: string, number, boolean
+
+// 5. Create a function with the two mandatory and one optional parameter
+
+// 6. Create a function which does not return anything
